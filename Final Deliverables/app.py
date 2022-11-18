034a8a3b77d9b10ac547b8a61a1eac6fecc9664b
DHANUSH-6 <dhanushpowerk6@gmail.com>
2022-11-18 22:42:45 +0530

    Add : Final Deliverables

--- Final Deliverables/app.py
+++ Final Deliverables/app.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 app.secret_key = 'f39c244d6c896864abe3310b839091799fed56007a438d637baf526007609fe0'
 
 # establish connection with IBM Db2 Database
-connection = ibm_db.connect(
-    "DATABASE=bludb;HOSTNAME=815fa4db-dc03-4c70-869a-a9cc13f33084.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30367;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=fzx82079;PWD=Gemfhl3b2DRTeUqB;", "", "")
+connection = ibm_db.connect("DATABASE=bludb;HOSTNAME=8e359033-a1c9-4643-82ef-8ac06f5107eb.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30120;SECURITY=SSL;UID=gyq42313;PWD=TRn8xYTPTGloNwQC;", "", "")
 
 load_dotenv()  # load keys from .env
 sg = sendgrid.SendGridAPIClient(api_key=os.environ.get(
@@ -22,13 +21,6 @@ from_email = Email("dhanushcodepro@gmail.com")
 # Handle expense model according to ibm db
 
 
-class Expense():
-    #db = ibm_db.prepare()
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(50), nullable=False)
-    expensename = db.Column(db.String(50), nullable=False)
-    amount = db.Column(db.Integer, nullable=False)
-    category = db.Column(db.String(50), nullable=False)
 
 
 @app.route('/')
@@ -38,15 +30,58 @@ def dashboard():
         # ask user to sign in if not done already
         return redirect(url_for('signin'))
     # Fetch the list of expenses from db
-    expenses = list()
+    sql = 'select * from expenses where cid = '+str(session['id'])
+    stmt = ibm_db.exec_immediate(connection, sql)   
+    
+    expense_list = {}
+    i=0
+    while (res:=ibm_db.fetch_assoc(stmt)) != False:
+        expense_list[i] = res
+        i+=1
     # go to homepage if signed in
-    return render_template('dashboard.html', pred=session['username'], data=expenses)
+    sql='select sum(eamount) from expenses where cid= '+str(session['id'])
+    stmt = ibm_db.exec_immediate(connection, sql)
+    sum_dict= ibm_db.fetch_assoc(stmt)
+    sum = sum_dict['1']
+
+    sql='select budget from users where id= '+str(session['id'])
+    stmt = ibm_db.exec_immediate(connection, sql)
+    budget_list= ibm_db.fetch_assoc(stmt)
+    # budget = budget_list.values()
+    for key,value in budget_list.items():
+        pass
+    rem = value - sum
+
+    per = (sum/value)*100
+    return render_template('dashboard.html', ex_list=expense_list,esum=sum,budget=value,rem=rem,per=per,name=session['username'])
 
 
-@app.route('/')
+
+
+@app.route('/addexpense')
 def add():
-    return render_template('add_expense.html')
+    return render_template('addexpense.html')
 
+@app.route('/addbudget')
+def addb():
+    sql='select budget from users where id= '+str(session['id'])
+    stmt = ibm_db.exec_immediate(connection, sql)
+    budget_list= ibm_db.fetch_assoc(stmt)
+    # budget = budget_list.values()
+    for key,value in budget_list.items():
+        pass
+    return render_template('addbudget.html',budget=value,id=session['id'])
+
+
+@app.route('/addbudget/<int:id>',methods=['POST'])
+def addbudget(id):
+    budget = request.form['budget']
+    sql = 'update users set budget = ? where id = '+str(id)
+    # Add to the database here
+    pstmt = ibm_db.prepare(connection, sql)
+    ibm_db.bind_param(pstmt, 1, budget)
+    ibm_db.execute(pstmt)
+    return redirect('/dashboard')
 
 @app.route('/addexpense', methods=['POST'])
 def addexpense():
@@ -54,21 +89,29 @@ def addexpense():
     expensename = request.form['expensename']
     amount = request.form['amount']
     category = request.form['category']
-
+    sql = 'INSERT INTO expenses(edate,ename,eamount,ecategory,cid) VALUES(?,?,?,?,?)'
     # Add to the database here
-    expense = Expense(date=date, expensename=expensename,
-                      amount=amount, category=category)
-    db.session.add(expense)
-    db.session.commit()
+    pstmt = ibm_db.prepare(connection, sql)
+    ibm_db.bind_param(pstmt, 1, date)
+    ibm_db.bind_param(pstmt, 2, expensename)
+    ibm_db.bind_param(pstmt, 3, amount)
+    ibm_db.bind_param(pstmt, 4, category)
+    ibm_db.bind_param(pstmt, 5, session['id'])
+    ibm_db.execute(pstmt)
+    flash('Expense added Successfully')
 
-    return redirect('/expenses')
+    return redirect('/dashboard')
 
 
-@app.route('/update/<int:id>')
+@app.route('/expense/update/<int:id>')
 def update(id):
     # Get from the database
-    expense = Expense.query.filter_by(id=id).first()
-    return render_template('update_expense.html', expense=expense)
+    sql = 'select * from expenses where id = '+str(id)
+    # Add to the database here
+    pstmt = ibm_db.prepare(connection, sql)
+    ibm_db.execute(pstmt)
+    acc = ibm_db.fetch_assoc(pstmt)
+    return render_template('updateexpense.html',acc=acc)
 
 
 @app.route('/edit', methods=['POST'])
@@ -79,50 +122,60 @@ def edit():
     amount = request.form['amount']
     category = request.form['category']
 
-    # Database part
-    expense = Expense.query.filter_by(id=id).first()
-    expense.date = date
-    expense.expensename = expensename
-    expense.amount = amount
-    expense.category = category
+    sql = 'update expenses set edate = ?,ename = ? ,eamount = ? ,ecategory = ? where id = '+str(id)
+    # Add to the database here
+    pstmt = ibm_db.prepare(connection, sql)
+    ibm_db.bind_param(pstmt, 1, date)
+    ibm_db.bind_param(pstmt, 2, expensename)
+    ibm_db.bind_param(pstmt, 3, amount)
+    ibm_db.bind_param(pstmt, 4, category)
+    ibm_db.execute(pstmt)
 
-    db.session.commit()
-    return redirect('/expenses')
+    
+    return redirect('/dashboard')
 
 
-@app.route('/delete/<int:id>')
+@app.route('/expense/delete/<int:id>', methods=['GET'])
 def delete(id):
     # Database operation
-    expense = Expense.query.filter_by(id=id).first()
-    db.session.delete(expense)
-    db.session.commit()
-    return redirect('/expenses')
+    # flash(str(id))
+    sql = 'delete from expenses where id = '+str(id)   #check if user is already registered
+    pstmt = ibm_db.prepare(connection, sql)
+    ibm_db.execute(pstmt)
+
+    return redirect('/dashboard')
 
 
 @app.route('/graph')
 def graph():
-    expenses = Expense.query.all()
-
+    
+    sql = 'select * from expenses where cid = '+str(session['id'])
+    stmt = ibm_db.exec_immediate(connection, sql)
+    expense_list = {}
+    i=0
+    while (res:=ibm_db.fetch_assoc(stmt)) != False:
+        expense_list[i] = res
+        i+=1
     total = 0
     household = 0
     food = 0
     entertainment = 0
     business = 0
     other = 0
-    for expense in expenses:
-        total += expense.amount
-        if expense.category == 'household':
-            household += expense.amount
-        elif expense.category == 'food':
-            food += expense.amount
-        elif expense.category == 'entertainment':
-            entertainment += expense.amount
-        elif expense.category == 'business':
-            business += expense.amount
-        elif expense.category == 'other':
-            other += expense.amount
+    for key,value in expense_list.items():
+        total += value['EAMOUNT']
+        if value['ECATEGORY'] == 'household':
+            household += value['EAMOUNT']
+        elif value['ECATEGORY'] == 'food':
+            food += value['EAMOUNT']
+        elif value['ECATEGORY'] == 'entertainment':
+            entertainment += value['EAMOUNT']
+        elif value['ECATEGORY'] == 'business':
+            business += value['EAMOUNT']
+        elif value['ECATEGORY'] == 'other':
+            other += value['EAMOUNT']
 
-    return render_template('graph.html', expenses=expenses, total=total, household=household, food=food, entertainment=entertainment, business=business, other=other)
+    return render_template('graph.html',  total=total, household=household, food=food, entertainment=entertainment, business=business, other=other)
 
 
 @app.route('/signout')
@@ -131,61 +184,55 @@ def signout():
     return redirect('/')
 
 
-@app.route('/register')
+@app.route('/signup')
 def register():
-    if 'username' in session:  # inform user if they're already signed in the same session
+    if 'username' in session:   #inform user if they're already signed in the same session
         flash('You are already signed in! Sign out to login with a different account')
         return redirect(url_for('dashboard'))
 
-    else:
-        # take user to the registration page
-        return render_template('signup.html')
+    else:    
+        return render_template('signup.html') #take user to the registration page
 
 
-@app.route('/regform', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def regform():
-    # get user details from the registration form
-    i = [i for i in request.form.values()]
-    uname = i[0]
-    uid = i[1]
-    pwd = i[2]
-
-    sql = 'SELECT * from donor WHERE email=?'  # check if user is already registered
+    uname = request.form['uname']   #get user id and password from the form
+    email = request.form['email']   
+    pwd = request.form['pass']
+    print(uname,email,pwd)
+    sql = 'SELECT * from users WHERE email=?'   #check if user is already registered
     pstmt = ibm_db.prepare(connection, sql)
-    ibm_db.bind_param(pstmt, 1, uid)
+    ibm_db.bind_param(pstmt, 1, email)
     ibm_db.execute(pstmt)
 
     acc = ibm_db.fetch_assoc(pstmt)
 
-    if acc:  # inform user to sign in if they have an existing account
+    if acc:     #inform user to sign in if they have an existing account
         flash('You are already a member. Please sign in using your registered credentials')
 
     else:
-        # insert credentials of new user to the database
-        sql = 'INSERT INTO donor VALUES(?,?,?)'
+        sql = 'INSERT INTO users(username,password,email) VALUES(?,?,?)' #insert credentials of new user to the database
         pstmt = ibm_db.prepare(connection, sql)
-        ibm_db.bind_param(pstmt, 1, uid)
+        ibm_db.bind_param(pstmt, 1, uname)
         ibm_db.bind_param(pstmt, 2, pwd)
-        ibm_db.bind_param(pstmt, 3, uname)
+        ibm_db.bind_param(pstmt, 3, email)
         ibm_db.execute(pstmt)
 
-        to_email = To(uid)  # set user as recipient for confirmation email
-        subject = "Welcome to GetPlasma"
-        content = Content("text/html", "<p>Hello " + uname +
-                          ",</p><p>Thank you for registering to the GetPlasma Application!</p><p>If this wasn't you, then immediately report to our <a href=\"mailto:getplasmaproject@gmail.com\">admin</a> or just reply to this email.</p>")
+        # to_email = To(uid)   #set user as recipient for confirmation email
+        # subject = "Welcome to GetPlasma"
+        # content = Content("text/html", "<p>Hello " + uname + ",</p><p>Thank you for registering to the GetPlasma Application!</p><p>If this wasn't you, then immediately report to our <a href=\"mailto:getplasmaproject@gmail.com\">admin</a> or just reply to this email.</p>")
 
-        email = Mail(from_email, to_email, subject,
-                     content)  # construct email format
-        email_json = email.get()  # get JSON-ready representation of the mail object
+        # email = Mail(from_email, to_email, subject, content) #construct email format
+        # email_json = email.get()    #get JSON-ready representation of the mail object
 
-        # send email by invoking an HTTP/POST request to /mail/send
-        response = sg.client.mail.send.post(request_body=email_json)
+        # response = sg.client.mail.send.post(request_body = email_json)  #send email by invoking an HTTP/POST request to /mail/send
 
-        flash(
-            'Registration Successful! Sign in using the registered credentials to continue')
 
-    # ask users to sign in after registration
-    return redirect(url_for('login'))
+        flash('Registration Successful! Sign in using the registered credentials to continue')
+
+
+    return redirect(url_for('signin'))  #ask users to sign in after registration
+
 
 
 @app.route('/signin')
@@ -193,16 +240,16 @@ def signin():
     if 'username' in session:  # inform user if they're already signed in the same session
         flash('You are already signed in! Sign out to login with a different account')
         return redirect(url_for('dashboard'))
-    return render_template('signin.html')  # take user to the sign in page
+    return render_template('login.html')  # take user to the sign in page
 
 
 @app.route('/signinform', methods=['POST'])
 def signinform():
-    uid = request.form['uid']  # get user id and password from the form
-    pwd = request.form['pwd']
+    uid = request.form['email']  # get user id and password from the form
+    pwd = request.form['pass']
 
     # check user credentials in the database
-    sql = 'SELECT uname from donor WHERE email=? AND pwd=?'
+    sql = 'SELECT * from users WHERE email=? AND password=?'
     pstmt = ibm_db.prepare(connection, sql)
     ibm_db.bind_param(pstmt, 1, uid)
     ibm_db.bind_param(pstmt, 2, pwd)
@@ -210,11 +257,14 @@ def signinform():
 
     acc = ibm_db.fetch_assoc(pstmt)
 
-    if acc:  # if the user is already registered to the application
-        session['username'] = acc['UNAME']
-        flash('Signed in successfully!')
+    if acc: #if the user is already registered to the application
+        session['username'] = acc['USERNAME']
+        session['id'] = acc['ID']
+        flash(session['username'] + str(session['id'])+'Signed in successfully!')
         return redirect(url_for('dashboard'))
-
-    else:  # warn upon entering incorrect credentials
+        
+    else:   #warn upon entering incorrect credentials
         flash('Incorrect credentials. Please try again!')
-        return render_template('signin.html')
+        return render_template('login.html')
+if __name__ == '__main__':
+    app.run(debug=True)
